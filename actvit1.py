3315a4dc762653f262093881807dd132de324a7f
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-11-01 21:47:49 +0530

    actvity3

--- actvit1.py
+++ actvit1.py
@@ -4,7 +4,7 @@
 num=int(input("enter a number "))
 
 #check if even or odd
-if num%2==0 : #when statement is true .
-print(num,"is even ")
+if num%2==0 : #when statement is true 
+     print(num,"is even ")
 else : # when statement is false 
-print(num,"is odd ")
+   print(num,"is odd ")
---
+++ actvit3.py
@@ -0,0 +1,13 @@
+#take  number from user 
+num1=int(input("enter number"))
+
+
+if num1 <50:
+   print("number is smaller than<50")
+         
+
+else: # when statement is false 
+    print("number is  gerater than >50")
+
+        
+  
